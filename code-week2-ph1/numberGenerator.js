65282a2ae7dd66222f5b42315c6ee427a7b94290
mark <markkixkithkim@gmail.com>
2024-04-09 12:34:36 +0300

    second commit

--- code-week2-ph1/numberGenerator.js
+++ code-week2-ph1/numberGenerator.js
@@ -1,17 +1,34 @@
-function logNumbersBetween(num1, num2) {
-    // this function will hold the comand that logs the numbers between specified values
-    if (num1 > num2) {
-      // Swap values depending on the greater number 
-      let temp = num1;
-      num1 = num2;
-      num2 = temp;
+function logNumbersBetweenPrompt() {
+      // Prompt the user to enter the first number
+      let num1 = parseInt(prompt("Enter the first number:"));
+    
+      // Check if num1 is a valid number
+      if (isNaN(num1)) {
+        console.log("Please enter a valid number for num1.");
+        return;
+      }
+    
+      // Prompt the user to enter the second number
+      let num2 = parseInt(prompt("Enter the second number:"));
+    
+      // Check if num2 is a valid number
+      if (isNaN(num2)) {
+        console.log("Please enter a valid number for num2.");
+        return;
+      }
+      if (num1 > num2) {
+        // Swap values depending on the greater number 
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+      }
+    
+      // The loop will work regardless of which number is greater
+      for (let i = num1; i <= num2; i++) {
+        console.log(i);
+      }
     }
-  
-    // we have to create a loop that runs untill the disired valure is obtained
-    for (let i = num1 ; i <= num2; i++) {
-      console.log(i);
-  
-    }
-  }
-  // allows for input of numbers that log out on the comsole
-    logNumbersBetween(1,10);
+    
+    // Call the function to log numbers between user-provided values
+    logNumbersBetweenPrompt();
+    
